258d9ee0e71a593eeb87c4effbfcea007b6be48f
ShaBree Quinones <shabreeq@gmail.com>
2018-12-18 11:00:22 -0500

    pseudo-coding

---
+++ assets/javascript/game.js
@@ -0,0 +1,12 @@
+// Create a random number for the computer 
+
+// Each crystal will be a button that generates a random number
+
+// If your sum equals the computers random number you will, if not you lose.
+
+var computerChoice = document.getElementById("computer").innerHTML = Math.random();
+  console.log(computerChoice);
+
+  var Wins =
+  var Losses =
+  var Score = 
